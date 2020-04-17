a06984f24de2302f137f480aae159f276a0fb7ae
RodrigoArboleda <rodrigo.arboleda@usp.br>
2020-04-17 17:03:27 -0300

    Criado arquivo com as bibliotecas do projeto

---
+++ disney.h
@@ -0,0 +1,14 @@
+#ifndef DISNEY
+#define DISNEY
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#define ERRO_SOCK "FALHA AO CRIAR SOCKET."
+
+typedef struct sockaddr_in sockaddr_ip;
+
+#endif
