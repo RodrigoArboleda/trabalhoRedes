784b6d6c047a8075a9659e97915f5c55d98b1f8c
Rodrigo Arboleda <rodrigo.arboleda@usp.br>
2020-04-30 19:48:03 -0300

    inicio de parada por erro

--- menssage.c
+++ menssage.c
@@ -1,31 +1,42 @@
 #include "menssage.h"
 
+/*
+A várivel a seguir mostra serve para saber se uma das threads, ou receber ou enviar
+foi encerrada, caso isso ocorra a outra encerra seu loop.
+*/
+int connect_status = 1;
+
 /*Esta função manda para o socket passado como parametros
 todas as mensagens escritas na entrada padrão
 @PARAMETROS
     - void *sock - endereço da variavel onde esta o socket
 @RETORNO
-    - void - sem retorno
+    - int - codigo de erro
 */
 void *send_menssage(void *sock){
 
     int sock_server = *((int*)sock);
 
+    int ret_thread = 0;
+
     int cod_error;
     char buffer[MENS_SIZE+1] = {0};
 
-    while (1)
+    while (connect_status)
     {
         scanf(" %4096[^\n]", buffer);
         cod_error = send(sock_server, buffer, MENS_SIZE, 0);
         if (cod_error <= 0)
         {
             error(ERROR_CONNECT);
-            exit(ERROR_CONNECT_COD);
+            ret_thread = ERROR_CONNECT_COD;
+            connect_status = 0;
+            pthread_exit(&ret_thread);
         }
     }
 
-    pthread_exit(NULL);
+    connect_status = 0;
+    pthread_exit(&ret_thread);
 }
 
 /*Esta função escuta indefinidamente a um socket e escreve na saida padrao
@@ -39,21 +50,26 @@ void *receive_menssage(void *sock){
 
     int sock_server = *((int*)sock);
 
+    int ret_thread = 0;
+
     int cod_error;
     char buffer[MENS_SIZE] = {0};
 
-    while (1)
+    while (connect_status)
     {
         cod_error = recv(sock_server, buffer, MENS_SIZE, 0);
         if (cod_error <= 0)
         {
             error(ERROR_CONNECT);
-            exit(ERROR_CONNECT_COD);
+            ret_thread = ERROR_CONNECT_COD;
+            connect_status = 0;
+            pthread_exit(&ret_thread);
         }
         if(strlen(buffer) != 0){
             printf("receive: %s\n", buffer);
         }
     }
 
-    pthread_exit(NULL);
+    connect_status = 0;
+    pthread_exit(&ret_thread);
 }
