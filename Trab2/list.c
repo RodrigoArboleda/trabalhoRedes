2c4b934176565f4899f159de85ba0cf7acde6fab
Rodrigo Arboleda <rodrigo.arboleda@usp.br>
2020-06-02 17:20:41 -0300

    codigo comentado

--- Trab2/list.c
+++ Trab2/list.c
@@ -1,5 +1,10 @@
 #include "list.h"
 
+/*
+Cria uma lista para armazenar os cliente do servidor
+@RETORNO
+    LIST* - Retorna o endereco da lista
+*/
 LIST* creat_list(){
     
     LIST* list_temp = NULL;
@@ -17,6 +22,16 @@ LIST* creat_list(){
     return list_temp;
 }
 
+/*
+Insere um elemento na lista
+@PARAMETROS
+    LIST* - list_d - o endereco da lista
+    CLIENT* - client_e - o endereco do cliente
+@RETORNO
+    int -   0  - elemento inserido na lista com sucesso
+            -5 - lista nao existe
+            -6 - elemento nao existe
+*/
 int insert_list(LIST* list_d, CLIENT* client_e){
     
     if (list_d == NULL)
@@ -31,6 +46,7 @@ int insert_list(LIST* list_d, CLIENT* client_e){
     
     now_element = list_d->listfirst;
     
+    /*verifica se a lista esta vazia*/
     if (now_element == NULL)
     {
         LIST_ELEMENT* new_list_element = NULL;
@@ -49,6 +65,7 @@ int insert_list(LIST* list_d, CLIENT* client_e){
         list_d->size = 1;
     }
     
+    /*caso a lista nao esteja vazia*/
     else
     {
         next_element = now_element->next;
@@ -79,6 +96,14 @@ int insert_list(LIST* list_d, CLIENT* client_e){
     return 0;
 }
 
+/*
+Remove um elemento da lista e rotorna seu endereco
+@PARAMETRO
+    LIST* list_d - endereco da lista
+    char* nickname - nome do cliente a ser removido da lista
+@RETORNO
+    CLIENT* - endereco do cliente
+*/
 CLIENT* remove_list(LIST* list_d, char* nickname){
 
     if (list_d == NULL)
@@ -107,7 +132,7 @@ CLIENT* remove_list(LIST* list_d, char* nickname){
         if(strcmp(now_element->client_ele->nickname, nickname) == 0){
             client_ret = now_element->client_ele;
             free(now_element);
-
+            /*caso seja o primeiro elemento a se removido*/
             if (previous_element == NULL)
             {
                 list_d->size = list_d->size -1;
@@ -129,7 +154,14 @@ CLIENT* remove_list(LIST* list_d, char* nickname){
     
 }
 
-
+/*
+Deleta a lista completamente
+@PARAMETRO
+    LIST* lisd_d - endereco da lista
+@RETORNO
+    int -   0 - lista deletada
+            -6 - lista nao existe
+*/
 int delete_list(LIST* list_d){
     
     if(list_d == NULL){
--- Trab2/list.h
+++ Trab2/list.h
@@ -18,12 +18,43 @@ typedef struct l{
 }LIST;
 
 
+/*
+Cria uma lista para armazenar os cliente do servidor
+@RETORNO
+    LIST* - Retorna o endereco da lista
+*/
 LIST* creat_list();
 
+/*
+Insere um elemento na lista
+@PARAMETROS
+    LIST* - list_d - o endereco da lista
+    CLIENT* - client_e - o endereco do cliente
+@RETORNO
+    int -   0  - elemento inserido na lista com sucesso
+            -5 - lista nao existe
+            -6 - elemento nao existe
+*/
 int insert_list(LIST* list_d, CLIENT* client_e);
 
+/*
+Remove um elemento da lista e rotorna seu endereco
+@PARAMETRO
+    LIST* list_d - endereco da lista
+    char* nickname - nome do cliente a ser removido da lista
+@RETORNO
+    CLIENT* - endereco do cliente
+*/
 CLIENT* remove_list(LIST* list_d, char* nickname);
 
+/*
+Deleta a lista completamente
+@PARAMETRO
+    LIST* lisd_d - endereco da lista
+@RETORNO
+    int -   0 - lista deletada
+            -6 - lista nao existe
+*/
 int delete_list(LIST* list_d);
 
 
